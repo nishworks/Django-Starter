2ccc6ba5244054552292e68a7512730dc3c0b8c8
Nishant Garg <nishgarg14@gmail.com>
2015-06-25 21:34:54 -0700

    Created initial project structure using tox

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools,os
+
+print os.path.dirname(os.path.realpath(__file__))
+#install_require = [line.strip() for line in 
+#                 open('pinned.txt')
+#                 if line.strip() and not line.strip().startswith('--')]
+
+setuptools.setup(
+    name='Voyage',
+      version='0.0.1',
+      description='Dashboard',
+      author='Nishant Garg',
+    #install_require=install_require,
+    )
